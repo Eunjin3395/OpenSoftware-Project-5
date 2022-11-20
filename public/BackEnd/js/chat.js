3b1b1acbdd86eb2c5d4a48c6c77746988d223fa5
Eunjin <jennyeunjin@khu.ac.kr>
2022-11-20 20:40:00 +0900

    login, lobby 부분 주석

---
+++ public/BackEnd/js/chat.js
@@ -0,0 +1,91 @@
+let roomTitle=document.getElementById('roomInfo_name')
+let roomMemNum=document.getElementById('roomInfo_num')
+let roomMemList=document.getElementById('memList')
+let roomList=document.getElementById('roomList')
+let lobbyRooms=document.getElementById('lobby_rooms')
+
+
+// 서버로부터 내가 입장한 방의 정보 받아서 표시 -> "this-room-info"를 listen
+// socket.emit("this-room-info",data)에 대한 listener
+// data = {room} (room은 내 방 객체)
+socket.on('this-room-info',(data)=>{
+  let thisRoom=data.room
+
+    // 내 방의 이름, 현재 인원수/limit, 멤버 리스트 표시 
+  roomTitle.innerText=thisRoom.roomname
+  roomMemNum.innerText=thisRoom.memNum+' / '+thisRoom.limit
+  thisRoom.memList.forEach(member => {
+    let item = document.createElement('li');
+    item.textContent = member;
+    roomMemList.appendChild(item);
+  });
+
+})
+
+// rooms array에 변화 생겼을 때 실시간으로 변화 반영
+// currentArea가 chat / lobby에 따라 다르게 처리
+// currentArea == chat이면 내 방의 정보와 전체 active한 방 리스트 갱신
+// currentArea == lobby이면 나는 아직 방에 입장하지 않은 상태, 전체 active한 방 리스트만 갱신
+socket.on('room update',rooms=>{
+  console.log('room update called')
+  if(currentArea=='chat'){ 
+    // rooms array에서 내 방 찾기
+    let thisRoom={};
+    roomname=roomTitle.innerText // html이 갖고있던 roomTitle로 현재 이 방의 roomname 구함
+    rooms.forEach(room=>{
+      if(room.roomname==roomname){
+        thisRoom=room
+        console.log(thisRoom)
+        // 자기 방의 이름, 멤버수/인원제한, 멤버 리스트 갱신
+        roomTitle.innerText=thisRoom.roomname
+        roomMemNum.innerText=thisRoom.memNum+' / '+thisRoom.limit
+        roomMemList.innerHTML=""
+        thisRoom.memList.forEach(member=>{
+          let item = document.createElement('li');
+          item.textContent=member
+          roomMemList.appendChild(item)
+        })
+      }
+    });
+    
+    // 전체 avtive한 채팅방 리스트 갱신 ( 방이름, 멤버수/인원제한 )
+    roomList.innerHTML=""
+    rooms.forEach(room => {
+      let item = document.createElement('li');
+      item.textContent = room.roomname+"    "+room.memNum+' / '+room.limit;
+      roomList.appendChild(item);
+    });
+
+  }else if(currentArea=="lobby"){
+    lobby_roomUpdate(rooms)
+  }
+})
+
+let messages = document.getElementById('messages');
+let messageForm = document.getElementById('messageForm');
+let messageInput = document.getElementById('messageInput');
+
+//msg 전송버튼 누르면
+messageForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    if (messageInput.value) {
+      socket.emit('chat message', messageInput.value);
+      messageInput.value = '';
+    }
+  });
+
+// 서버로부터 chat 받음
+socket.on('chat message', function(data) {
+  let item = document.createElement('li');
+  item.textContent = `${data.name} : ${data.msg}`;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+// 서버로부터 notify 받음
+socket.on('notify-message', function(msg) {
+  let item = document.createElement('li');
+  item.textContent = msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
---
+++ public/BackEnd/js/lobby.js
@@ -0,0 +1,93 @@
+//secret 여부 클릭 시 secretCode 받는 창 띄우기
+const radios = document.querySelectorAll('input[type=radio][name="isSecret"]');
+radios.forEach(radio=>radio.addEventListener('change',()=>{
+  if(radio.value=='Y'){
+    document.getElementById('secretCodeArea').className=""
+  }else{
+    document.getElementById('secretCodeArea').className="d-none"
+    document.getElementById('createSecretCode').value=""
+  }
+}
+
+))
+
+
+// 방 만들기 -> "create-room"을 emit
+// socket.emit("create-room", data)
+// data ={roomname,isSecret,secretCode,limit}
+let roomCreateForm = document.getElementById('roomCreateForm');
+roomCreateForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  let roomTitleInput = document.getElementById('createRoomTitle').value;
+  let isSecretYN=document.querySelector('input[type=radio][name=isSecret]:checked').value
+  let secretCode=""
+  let roomLimit = document.getElementById('createRoomLimit').value;
+
+  if(isSecretYN=="Y"){ // 비밀방인 경우 비밀코드 지정
+    secretCode=document.getElementById('createSecretCode').value;
+  }
+  if(roomTitleInput==""){ // 방 이름 입력 안한 경우
+    alert("Enter room title")
+    return;
+  }
+  if(isSecretYN=="Y"&&secretCode==""){ //비밀방으로 한다고 했는데 비밀코드 입력 안한 경우
+    alert("Enter secret code")
+    return;
+  }
+  if(roomLimit>99||roomLimit<0){ // 인원제한의 입력 정상적인지 체크
+    alert("Enter valid room limit (0~99)")
+    return;
+  }
+
+  // 모든 입력 정상적인 경우, 서버로 data 전송
+  socket.emit('create-room',{
+    roomname:roomTitleInput,
+    isSecret:isSecretYN,
+    secretCode:secretCode,
+    limit:roomLimit
+  })
+});
+
+
+// 방 생성 결과 & 방 입장 -> "room-create-result"를 listen하고 room_in 호출
+// socket.emit("room-create-result",data)에 대한 listener
+// data = {roomname,result,msg}
+socket.on('room-create-result',(data)=>{
+  if(data.result){ // 방 생성 성공 시 room_in 함수 호출
+    alert(data.msg)
+    // 방장이 방 생성 후 바로 입장하므로 limit과 비밀코드 체크하지 않고 room_in 호출
+    room_in(data.roomname)
+  }else{ // 방 생성 실패
+    alert(data.msg)
+  }
+})
+
+
+// 입력받은 roomname을 가진 채팅방으로 입장 -> "room-in"을 emit하고 chatArea 보여주기 및 currentArea 변경
+// socket.emit("room-in",roomname)
+function room_in(roomname){
+  document.getElementById('lobbyArea').className="d-none"
+  document.getElementById('chatArea').className=""
+  socket.emit("room-in",roomname)
+  currentArea='chat'
+}
+
+
+// lobby의 room list를 update하는 함수 (입장하기 onClick eventlistenr 포함)
+function lobby_roomUpdate(rooms){
+  lobbyRooms.innerHTML=""
+  rooms.forEach(room => {
+    let item = document.createElement('li');
+    item.textContent = `${room.roomname}    ${room.memNum} / ${room.limit}   입장하기`;
+    item.onclick=function(){
+      console.log('onClick roomname: ',room.roomname)
+
+      // room in 시키기 전에 입장할 room의 인원제한 체크해서 내가 들어갈 수 있는지 체크 필요
+      // room in 시키기 전에 입장할 room이 비밀방이라면 비밀코드 체크 절차 필요
+
+      room_in(room.roomname)
+    }  
+      
+    lobbyRooms.appendChild(item);
+  })
+}
